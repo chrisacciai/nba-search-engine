c57776f98edc2a0e713dd6de5c01da5ee4210936
Christopher Acciai <christopheracciai@Christophers-MacBook-Pro.local>
2019-11-01 17:06:16 -0400

    Filter style changes

--- Arena.php
+++ Arena.php
@@ -8,6 +8,26 @@
     <link rel="stylesheet" href="./homepage.css">
 
     <title>NBA Search Engine</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script>
+      function filterConference(str) {
+        if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("table-component").innerHTML = this.responseText;
+            }
+        }
+
+        xmlhttp.open("GET","filterConference.php?q="+str,true);
+        xmlhttp.send();
+      }
+    </script>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -18,11 +38,8 @@
         <div class="media">
           <img class="logo" src="./nbalogo.png" class="mr-3" alt="logo" height="44" width="44">
         </div>
-        <a class="navbar-brand" href="#">NBA Search Engine</a>
+        <a class="navbar-brand" href="index.html">NBA Search Engine</a>
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-          </li>
           <li class="nav-item active">
             <a class="nav-link" href="Arena.php">Arena</a>
           </li>
@@ -52,64 +69,36 @@
       </div>
     </nav>
     <br>
-    <div class="sort-menu">
-            <form>
-                <label class="sort-text">Sort:</label>
-                <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          Alphabetical
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                          <a class="dropdown-item" href="#">Alphabetical</a>
-                          <a class="dropdown-item" href="#">Capacity</a>
-                        </div>
-                      </div>
-            </form>
+    <div class="filter-border">
+      <div class="filter-items">
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Alphabetical
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Alphabetical</a>
+            <a class="dropdown-item" href="#">Capacity</a>
+          </div>
         </div>
-    <div class="filter-menu">
-        <form>
-            <label class="filter-text">Filter:</label>
-            <div class="form-group">
-            <label class="division-radio" for="formControlDivision">Division</label>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1">
-                    <label class="form-check-label" for="gridRadios1">
-                      North
-                    </label>
-                </div>
-                <div class="form-check">
-                        <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
-                        <label class="form-check-label" for="gridRadios2">
-                          South
-                        </label>
-                    </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3">
-                    <label class="form-check-label" for="gridRadios3">
-                      East
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios4" value="option4">
-                    <label class="form-check-label" for="gridRadios4">
-                      West
-                    </label>
-                </div>
-            </div>
-            <button type="submit" class="btn btn-primary mb-2 btn-sm">Apply Filter</button>
-        </form>
+        <div class="form-group">
+        <label class="division-radio" for="formControlDivision">Conference</label>
+          <div class="form-check">
+              <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="Eastern" onclick="filterConference(this.value);">
+              <label class="form-check-label" for="gridRadios3">
+                East
+              </label>
+          </div>
+          <div class="form-check">
+              <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios4" value="Western" onclick="filterConference(this.value);">
+              <label class="form-check-label" for="gridRadios4">
+                West
+              </label>
+          </div>
+        </div>
+      </div>
     </div>
-    <table class="table-center">
-      <thead>
-        <tr>
-          <th>Arena Name</th>
-          <th>City</th>
-          <th>State</th>
-          <th>Team Name</th>
-          <th>Capacity</th>
-        </tr>
-      </thead>
-        <?php
+    <div id="table-component">
+      <?php
           include_once("./library.php"); // To connect to the database
           $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
           // Check connection
@@ -117,15 +106,26 @@
           {
           echo "Failed to connect to MySQL: " . mysqli_connect_error();
           }
-          $sql = "SELECT a.name AS arena_name, a.city, a.state, t.name AS team_name, a.capacity FROM Arena AS a LEFT JOIN (SELECT * FROM Team NATURAL JOIN Plays_At) AS t ON a.arena_id = t.arena_id";
+          $sql = "SELECT a.name AS arena_name, a.city, a.state, t.name AS team_name, a.capacity FROM Arena AS a JOIN (SELECT * FROM Team NATURAL JOIN Plays_At) AS t ON a.arena_id = t.arena_id";
           $result = mysqli_query($con,$sql);
+          echo "<table class='table-center'>
+          <thead>
+          <tr>
+              <th>Arena Name</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Team Name</th>
+              <th>Capacity</th>
+          </tr>
+          </thead>";
           // Print the data from the table row by row
           while($row = mysqli_fetch_array($result)) {
-            echo "<tr><td>". $row['arena_name'] ."</td><td>". $row['city'] ."</td><td>". $row['state'] ."</td><td>". $row['team_name'] ."</td><td>". $row['capacity'] ."</td></tr>";
-            }
-            mysqli_close($con);
-        ?>
-    </table>
+              echo "<tr><td>". $row['arena_name'] ."</td><td>". $row['city'] ."</td><td>". $row['state'] ."</td><td>". $row['team_name'] ."</td><td>". $row['capacity'] ."</td></tr>";
+              }
+          echo "</table>";
+          mysqli_close($con);
+      ?>
+    </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>

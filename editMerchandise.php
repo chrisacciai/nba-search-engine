9e782f68c2a2e245db233ffe22e5bc197e89d448
Casey Simon <css8cw@virginia.edu>
2019-11-14 12:52:03 -0500

    Formatted edit.html a bit and editMerchandise.php

--- editMerchandise.php
+++ editMerchandise.php
@@ -20,14 +20,10 @@
                     // code for IE6, IE5
                     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
                 }
-                xmlhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        document.getElementById("table-component").innerHTML = this.responseText;
-                    }
-                }
+                    
+                xmlhttp.open("GET","insertMerchandise.php?arena_id="+document.getElementById("arena_id").value+"&name="+document.getElementById("name").value+"&city="+document.getElementById("city").value+"&state="+document.getElementById("state").value+"&capacity="+document.getElementById("capacity").value, true);
+                    xmlhttp.send();
                 document.write(document.getElementById("arena_id").value);
-                 #xmlhttp.open("GET","insertMerchandise.php?arena_id="+document.getElementById("arena_id").value+"&name="+document.getElementById("name").value+"&city="+document.getElementById("city").value+"&state="+document.getElementById("state").value+"&capacity="+document.getElementById("capacity").value, true);
-                #xmlhttp.send();
             }
         </script>
 
@@ -66,28 +62,29 @@
                         <a class="nav-link" href="#">Vendors</a>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
             </div>
         </nav>
         <br>
         <div class="home-titles">
-            <p class="home-title">Insert into/delete from Merchandise table</p>
+            <p class="home-title">Edit Merchandise table</p>
+            </br>
         </div>
-        <div>
-            Arena ID: <input id="arena_id"></input>
-            Name: <input id="name"></input>
-            City: <input id="city"></input>
-            State: <input id="state"></input>
-            Capacity: <input id="capacity"></input>
-        </div>
-        <div>
-            <button onclick="insert()">Insert into table</button>
+        <div style="text-align: center">
+            <form action="insertMerchandise.php" method="post">
+                <div>
+                    Item ID: <input type="text" name="item_id"></input>
+                </div>
+                </br>
+                <div>
+                    Type: <input type="text" name="type"></input>
+                </div>
+                </br>
+                <div>
+                    <input class="btn btn-outline-success my-2 my-sm-0" type="Submit" value="Insert"></input>
+                </div>
+            </form>
         </div>
             
-
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
